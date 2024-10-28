771c51976ad212320fd174789a62d6451410d063
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-10-28 16:57:37 +0600

    add login

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./api/baseApi";
+import authReducer from "./features/auth/authSlice";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import counterReducer from "./features/counter/counterSlice";
+
+const parsistConfig = {
+  key: "auth",
+  storage,
+};
+
+const parsistedAuthReducer = persistReducer(parsistConfig, authReducer);
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: parsistedAuthReducer,
+    counter: counterReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
