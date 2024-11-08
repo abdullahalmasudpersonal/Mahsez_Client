89ba75dfed11bbd506386fae84fc5404a13af2c3
unknown <abdullahalmasud01737906772@gmail.com>
2024-11-08 15:40:01 +0600

    update my profle

---
+++ src/pages/dashboard/myOrder/MyOrder.tsx
@@ -0,0 +1,15 @@
+import PageTitle from "../../shared/PageTitle/PageTitle";
+
+const MyOrder = () => {
+  return (
+    <div className="dashboard-dev2">
+      <PageTitle pageTitle="My Profile " />
+      <div className="pt-4 px-4">
+        <h4 className="fw-bold ">My Orders</h4>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default MyOrder;
