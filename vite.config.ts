84c4caa1f99505563cb27017b09530af50edf33a
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-08-10 01:14:47 +0600

    build success

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   define: {
     "process.env": {}, // এটি মূলত একটি খালি অবজেক্ট দিয়ে দেয়া হচ্ছে
   },
