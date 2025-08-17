1138691116ff8d11b60b7505c83fc6c4a560a475
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-08-18 02:16:37 +0600

    another update

--- src/pages/shared/protectedRoute/RequireAdmin.tsx
+++ src/pages/shared/protectedRoute/RequireAdmin.tsx
@@ -1,17 +1,25 @@
-// import { useLocation } from "react-router-dom";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { logout, selectCurrentUser } from "../../../redux/features/auth/authSlice";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import { Navigate, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const RequireAdmin = ({ children }: { children: ReactNode }) => {
   const user = useAppSelector(selectCurrentUser);
   const location = useLocation();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (!user || user.role !== "admin") {
+      dispatch(logout());
+      navigate("/auth/login", { state: { from: location }, replace: true });
+    }
+  }, [user, dispatch, navigate, location]);
+
 
   if (!user || user.role !== "admin") {
-    dispatch(logout());
-    return <Navigate to="/auth/login" state={{ from: location }} replace />;
+    return null;
   }
   return children;
 };
